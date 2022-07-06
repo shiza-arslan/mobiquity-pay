1a1324bea8635ab86b74410eca0276845c470cd1
Shafiq Rehman <shafiq.rehman@trangolabs.com>
2022-07-06 10:37:38 +0500

    error popup bug fixing

--- src/app/core/components/error-popup/error-popup.component.ts
+++ src/app/core/components/error-popup/error-popup.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-error-popup',
   templateUrl: './error-popup.component.html',
   styleUrls: ['./error-popup.component.scss'],
 })
 export class ErrorPopupComponent implements OnInit {
-  constructor() {}
+  @Input() public errorMessage: any = '';
+  constructor(public activeModal: NgbActiveModal, private modalSerivce: NgbModal) {}
 
   ngOnInit(): void {}
+  close() {
+    this.activeModal.dismiss();
+  }
 }
