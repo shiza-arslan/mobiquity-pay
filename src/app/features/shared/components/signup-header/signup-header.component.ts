08d213428b13777debd5615d860a3d8ebe85d93e
Shafiq Rehman <shafiq.rehman@trangolabs.com>
2022-07-07 10:26:51 +0500

    Signup Header

--- src/app/features/shared/components/signup-header/signup-header.component.ts
+++ src/app/features/shared/components/signup-header/signup-header.component.ts
@@ -1,17 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from '../../../pre-auth/components/login/login.component';
+import { UowService } from '../../../../data-acsess/uow.service';
 @Component({
   selector: 'mobiquity-pay-signup-header',
   templateUrl: './signup-header.component.html',
   styleUrls: ['./signup-header.component.scss'],
 })
 export class SignupHeaderComponent implements OnInit {
-  constructor(private modalSerivce: NgbModal) {}
+  selectedLanguage: any = 'en';
+  languages = [
+    {
+      label: 'English',
+      lang: 'en',
+    },
+    {
+      label: 'Arabic',
+      lang: 'ar',
+    },
+    {
+      label: 'French',
+      lang: 'french',
+    },
+    {
+      label: 'Spanish',
+      lang: 'spanish',
+    },
+  ];
+  constructor(private modalSerivce: NgbModal, private service: UowService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (localStorage.getItem('language')) {
+      this.selectedLanguage = localStorage.getItem('language');
+    }
+  }
 
   openModal() {
     const modalRef = this.modalSerivce.open(LoginComponent, { animation: false, backdrop: false });
+  }
+  changeLanguage(lang: string) {
+    this.service.translateService.setLang(lang);
+    this.selectedLanguage = lang;
+    localStorage.setItem('language', lang);
   }
 }
