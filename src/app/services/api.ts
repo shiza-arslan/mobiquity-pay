e58a184ae3d3a6de6a65ef24651de61bf7de3834
Shafiq Rehman <shafiq.rehman@trangolabs.com>
2022-07-14 10:58:39 +0500

    changes in api service file

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,133 +1,35 @@
-import { Observable } from 'rxjs';
-import { ApiUtils } from './api-utils';
-import { IApiRequest } from './api.interface';
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-/**
- * Api Base class.
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export class Api<TParams = any, TData = any> {
-  constructor(protected apiUtils: ApiUtils, private endpoint: string) {}
+@Injectable()
+export class Api {
+  constructor(private http: HttpClient) {}
 
-  /**
-   * Performs a GET request including paging parameter.
-   * @param params Query Params.
-   * @param subEntity Request specific sub path.
-   * @param headers Http Headers.
-   * @param noAuth Indicator auth.
-   * @param responseType Response type.
-   * @param observe Observe type.
-   * @return A stream of data from the API response.
-   */
-  public get<Params = TParams, Data = TData>({
-    params,
-    subEntity = '',
-    headers,
-    noAuth = false,
-    responseType = 'json',
-    observe = 'body',
-  }: IApiRequest): Observable<Data> {
-    return this.apiUtils.send<Data>({
-      method: 'GET',
-      path: `${this.endpoint}${ApiUtils.safeURIAppend(subEntity)}`,
-      headers,
-      params,
-      noAuth,
-      responseType,
-      observe,
-    });
+  private formatErrors(error: any) {
+    return throwError(error.error);
   }
 
-  /**
-   * Performs a POST request including paging parameter.
-   * @param params Query Params.
-   * @param subEntity Request specific sub path.
-   * @param headers Http Headers.
-   * @param noAuth Indicator auth.
-   * @param body Request body.
-   * @param responseType Response type.
-   * @param observe Observe type.
-   * @return A stream of data from the API response.
-   */
-  public post<Body = TParams, Data = TData>({
-    params,
-    subEntity = '',
-    headers,
-    noAuth = false,
-    body,
-    responseType = 'json',
-    observe = 'body',
-  }: IApiRequest): Observable<Data> {
-    return this.apiUtils.send<Data>({
-      method: 'POST',
-      path: `${this.endpoint}${ApiUtils.safeURIAppend(subEntity)}`,
-      headers,
-      params,
-      noAuth,
-      body,
-      observe,
-      responseType,
-    });
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get(`${path}`, { params }).pipe(catchError(this.formatErrors));
   }
 
-  /**
-   * Performs a PUT request including paging parameter.
-   * @param params Query Params.
-   * @param subEntity Request specific sub path.
-   * @param headers Http Headers.
-   * @param noAuth Indicator auth.
-   * @param body Request body.
-   * @param responseType Response type.
-   * @param observe Observe type.
-   * @return A stream of data from the API response.
-   */
-  public put<Body = TParams, Data = TData>({
-    params,
-    subEntity = '',
-    headers,
-    noAuth = false,
-    body,
-    responseType = 'json',
-    observe = 'body',
-  }: IApiRequest): Observable<Data> {
-    return this.apiUtils.send<Data>({
-      method: 'PUT',
-      path: `${this.endpoint}${ApiUtils.safeURIAppend(subEntity)}`,
-      headers,
-      params,
-      noAuth,
-      responseType,
-      observe,
-      body,
-    });
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(`${path}`, JSON.stringify(body)).pipe(catchError(this.formatErrors));
   }
 
-  /**
-   * Performs a DELETE request including paging parameter.
-   * @param params Query Params.
-   * @param subEntity Request specific sub path.
-   * @param headers Http Headers.
-   * @param noAuth Indicator auth.
-   * @param responseType Response type.
-   * @param observe Observe type.
-   * @return A stream of data from the API response.
-   */
-  public delete<Params = TParams, Data = TData>({
-    params,
-    subEntity = '',
-    headers,
-    noAuth = false,
-    responseType = 'json',
-    observe = 'body',
-  }: IApiRequest): Observable<Data> {
-    return this.apiUtils.send<Data>({
-      method: 'DELETE',
-      path: `${this.endpoint}${ApiUtils.safeURIAppend(subEntity)}`,
-      headers,
-      params,
-      responseType,
-      observe,
-      noAuth,
-    });
+  post(path: string, body: Object = {}): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Accept: '*/*',
+      }),
+    };
+    return this.http.post(`${path}`, JSON.stringify(body), httpOptions).pipe(catchError(this.formatErrors));
+  }
+
+  delete(path: any): Observable<any> {
+    return this.http.delete(`${path}`).pipe(catchError(this.formatErrors));
   }
 }
