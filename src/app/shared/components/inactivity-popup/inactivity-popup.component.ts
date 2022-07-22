9b68759c6488de2c065b0f7af9c9794e79c2bba9
Shafiq U Rehman <shafiquog@gmail.com>
2022-07-22 14:10:24 +0500

    improvemnets and bug fixing

---
+++ src/app/shared/components/inactivity-popup/inactivity-popup.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { LoginComponent } from 'src/app/features/PreLogin/component/login/login.component';
+
+@Component({
+  selector: 'app-inactivity-popup',
+  templateUrl: './inactivity-popup.component.html',
+  styleUrls: ['./inactivity-popup.component.scss'],
+})
+export class InactivityPopupComponent implements OnInit {
+  constructor(
+    private route: Router,
+    private matDialog: MatDialog,
+    private dialogRef: MatDialogRef<InactivityPopupComponent>,
+  ) {}
+
+  ngOnInit(): void {
+    // this.closeModal();
+    // console.log('hello close modal');
+  }
+
+  keepaLive() {
+    this.dialogRef.close('true');
+  }
+  login() {
+    this.closeModal();
+    sessionStorage.removeItem('isLoggedIn');
+    this.matDialog.open(LoginComponent);
+  }
+
+  closeModal() {
+    this.dialogRef.close();
+  }
+}
